48e6779dbc2b87b37951665d5268877b734882f2
Puneet Jain <59646366+puneetj1993@users.noreply.github.com>
2022-04-20 17:20:23 +0530

    BFS and DFS

---
+++ DSA-in-Python-master/DSA-in-Python-master/BFS and DFS .py
@@ -0,0 +1,61 @@
+"""
+P.S:-  Depth First Search 
+"""
+
+
+# Using a Python dictionary to act as an adjacency list in case of graph
+# This dictionary shows the how nodes are connected to their parents
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+}
+
+visited = set() # Set to keep track of visited nodes.
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+dfs(visited, graph, 'A')
+
+"""
+P.S.:-  BFS - Breath First Search
+"""
+
+# Using a Python dictionary to act as an adjacency list in case of graph
+# This dictionary shows the how nodes are connected to their parents
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+visited = [] # List to keep track of visited nodes.
+queue = []     #Initialize a queue
+
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
+
+# Driver Code
+bfs(visited, graph, 'A')
